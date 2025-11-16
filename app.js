9de468419a17a440dc5a3220a6b601731d91a5b5
Himanshu Bokde <himanshubokde09@gmail.com>
2025-11-16 18:06:42 +0530

    Handle Port

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get("/status", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
+
